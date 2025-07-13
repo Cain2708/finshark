82067a36f518becc07a107978797371373a7f2f4
Cain2708 <cu.18bcs2011@gmail.com>
2025-07-14 01:31:21 +0530

    api done

---
+++ api/Migrations/20250713194357_SeedRole.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8cc62825-b397-45d3-b7c5-e40ab01071da");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9dbd037a-527c-4836-a693-3f24e88385b3");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "90ee7213-ec2f-4fe4-ba9d-8544b75b42d1", null, "User", "USER" },
+                    { "c73d146d-055c-4c7a-96c8-b615b440161d", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "90ee7213-ec2f-4fe4-ba9d-8544b75b42d1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c73d146d-055c-4c7a-96c8-b615b440161d");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "8cc62825-b397-45d3-b7c5-e40ab01071da", null, "User", "USER" },
+                    { "9dbd037a-527c-4836-a693-3f24e88385b3", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
